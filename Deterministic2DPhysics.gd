acdc37afa4dada93ea67339aac08cd251a75ede3
FlashBash21 <nathanolson55379@gmail.com>
2025-07-12 01:52:07 -0500

    Switch Local Directory

--- Deterministic2DPhysics.gd
+++ Deterministic2DPhysics.gd
@@ -4,9 +4,11 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
+	# add_custom_type(yada yada)
 	pass
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
+	# remove_custom_type(yada yada)
 	pass
